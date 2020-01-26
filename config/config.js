f2edfddaca45ddf77182fdf047688b0559424734
Alec Down <alecdown@gmail.com>
2020-01-26 12:36:05 -0600

    configures DB for production

--- config/config.js
+++ config/config.js
@@ -16,10 +16,10 @@ module.exports = {
     logging: false
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
+    username: process.env.DB_PROD_USERNAME,
+    password: process.env.DB_PROD_PASSWORD,
+    database: process.env.DB_PROD_DATABASE,
+    host: process.env.DB_PROD_HOST,
     dialect: 'mysql',
     logging: false
   }
